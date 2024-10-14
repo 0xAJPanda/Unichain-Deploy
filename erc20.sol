376369725b612c29c2df1e2ed4e68d9aff79b408
0xAJPanda <0xnftnut@gmail.com>
2024-10-14 16:42:16 -0500

    an other one

--- erc20.sol
+++ erc20.sol
@@ -2,18 +2,16 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract Console is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
-    constructor(address initialOwner)
-        ERC20("console", "CON")
-        Ownable(initialOwner)
-        ERC20Permit("console")
-    {
+    constructor(
+        address initialOwner
+    ) ERC20("console", "CON") Ownable(initialOwner) ERC20Permit("console") {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
@@ -31,10 +29,11 @@ contract Console is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Pausable)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
         super._update(from, to, value);
     }
 }
